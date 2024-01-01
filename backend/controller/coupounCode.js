23f213ce3f8087d020421edbd173c3d405ceaf32
06776 <hajdarapatrik11@outlook.com>
2024-01-01 18:24:03 +0100

    Create and modify coupounCode.js

---
+++ backend/controller/coupounCode.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Shop = require("../model/shop");
+const ErrorHandler = require("../utils/ErrorHandler");
+const { isSeller } = require("../middleware/auth");
+const CoupounCode = require("../model/coupounCode");
+const router = express.Router();
+
+router.post(
+  "/create-coupon-code",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const isCoupounCodeExists = await CoupounCode.find({
+        name: req.body.name,
+      });
+      if (isCoupounCodeExists.length !== 0) {
+        return next(new ErrorHandler("Nem megfelelő kuponkód", 400));
+      }
+      const coupounCode = await CoupounCode.create(req.body);
+      res.status(201).json({
+        success: true,
+        coupounCode,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+router.get(
+  "/get-coupon/:id",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const couponCodes = await CoupounCode.find({ shopId: req.seller.id });
+      res.status(201).json({
+        success: true,
+        couponCodes,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+router.delete(
+  "/delete-coupon/:id",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const couponCode = await CoupounCode.findByIdAndDelete(req.params.id);
+      if (!couponCode) {
+        return next(new ErrorHandler("Nincs ilyen kuponkód", 400));
+      }
+      res.status(201).json({
+        success: true,
+        message: "Sikeres törlés",
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+router.get(
+  "/get-coupon-value/:name",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const couponCode = await CoupounCode.findOne({ name: req.params.name });
+      res.status(200).json({
+        success: true,
+        couponCode,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+module.exports = router;
