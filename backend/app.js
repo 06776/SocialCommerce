2a5d2ce48e6ad261e3050728b9ea162eac2168ff
06776 <hajdarapatrik11@outlook.com>
2024-01-01 18:34:33 +0100

    Create and modify app.js

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+const user = require("./controller/user");
+const shop = require("./controller/shop");
+const product = require("./controller/product");
+const coupon = require("./controller/coupounCode");
+const payment = require("./controller/payment");
+const order = require("./controller/order");
+
+app.use("/api/v2/user", user);
+app.use("/api/v2/order", order);
+app.use("/api/v2/shop", shop);
+app.use("/api/v2/product", product);
+app.use("/api/v2/coupon", coupon);
+app.use(ErrorHandler);
+
+module.exports = app;
