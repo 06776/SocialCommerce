75b70227cb121bf286a6505bc7b26892b234c546
06776 <hajdarapatrik11@outlook.com>
2024-01-01 18:33:59 +0100

    Create and modify shopToken.js

---
+++ backend/utils/shopToken.js
@@ -0,0 +1,16 @@
+const sendShopToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  };
+  res.status(statusCode).cookie("seller_token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
+
+module.exports = sendShopToken;
