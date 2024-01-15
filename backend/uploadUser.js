234d8e2c56fd675d5a740b05f4ec79a0aaf45e51
06776 <hajdarapatrik11@outlook.com>
2024-01-15 17:23:06 +0100

    Create uploadUser.js and testing the DB with test datas

---
+++ backend/uploadUser.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const User = require("./model/user");
+
+mongoose
+  .connect(
+    "mongodb+srv://socialcommerce:W3K3hjztCrft@socialcommerce.xuhc4r5.mongodb.net/SocialCommerce",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Sikeresen csatlakozva a MongoDB-hez");
+
+    const userData = {
+      name: "Minta Péter",
+      email: "mintapeter@gmail.com",
+      password: "Teszt1234",
+      phoneNumber: 123456789,
+      addresses: [
+        {
+          country: "Hungary",
+          city: "Budapest",
+          address1: "Teszt utca",
+          address2: "12",
+          zipCode: 1234,
+          addressType: "Home",
+        },
+      ],
+      role: "user",
+      avatar: {
+        public_id: "example-public-id",
+        url: "example-avatar-url",
+      },
+    };
+
+    const user = new User(userData);
+    user
+      .save()
+      .then(() => {
+        console.log("A felhasználó létre hozva és mentve az adatbázisba");
+      })
+      .catch((error) => {
+        console.error("Hiba történt a felhasználó adatainak mentése során: ", error.message);
+      })
+      .finally(() => {
+        mongoose.connection.close();
+      });
+  })
+  .catch((error) => {
+    console.error("Hiba történt a MongoDB-hez való csatlakozás során: ", error.message);
+  });
