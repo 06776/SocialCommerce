4ef24819a197fb05024be79c29cfa113132db680
06776 <hajdarapatrik11@outlook.com>
2024-02-12 21:10:50 +0100

    Changes in couponCode.js model

--- backend/model/coupounCode.js
+++ backend/model/coupounCode.js
@@ -6,23 +6,29 @@ const coupounCodeSchema = new mongoose.Schema({
     required: [true],
     unique: true,
   },
+
   value: {
     type: Number,
     required: true,
   },
+
   minAmount: {
     type: Number,
   },
+
   maxAmount: {
     type: Number,
   },
+
   shopId: {
     type: String,
     required: true,
   },
+
   selectedProduct: {
     type: String,
   },
+
   createdAt: {
     type: Date,
     default: Date.now(),
