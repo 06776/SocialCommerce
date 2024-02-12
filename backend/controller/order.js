d55bf2859d6e01d6d92a2844c3c07e8c0c320991
06776 <hajdarapatrik11@outlook.com>
2024-02-12 21:02:53 +0100

    Changes in order.js

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -121,60 +121,6 @@ router.put(
   })
 );
 
-router.put(
-  "/order-refund/:id",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const order = await Order.findById(req.params.id);
-      if (!order) {
-        return next(new ErrorHandler("Nincs ilyen rendelés", 400));
-      }
-      order.status = req.body.status;
-      await order.save({ validateBeforeSave: false });
-      res.status(200).json({
-        success: true,
-        order,
-        message: "Order Refund Request successfully!",
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
-);
-
-router.put(
-  "/order-refund-success/:id",
-  isSeller,
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const order = await Order.findById(req.params.id);
-      if (!order) {
-        return next(new ErrorHandler("Order not found with this id", 400));
-      }
-      order.status = req.body.status;
-      await order.save();
-      res.status(200).json({
-        success: true,
-        message: "Order Refund successfull!",
-      });
-      if (req.body.status === "Refund Success") {
-        order.cart.forEach(async (o) => {
-          await updateOrder(o._id, o.qty);
-        });
-      }
-
-      async function updateOrder(id, qty) {
-        const product = await Product.findById(id);
-        product.stock += qty;
-        product.sold_out -= qty;
-        await product.save({ validateBeforeSave: false });
-      }
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
-);
-
 router.get(
   "/admin-all-orders",
   isAuthenticated,
