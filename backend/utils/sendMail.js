58bbbf3d5a0f599f760872278f50ee10e01102ed
06776 <hajdarapatrik11@outlook.com>
2024-02-12 21:19:14 +0100

    Small changes in sendMail.js

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -10,12 +10,14 @@ const sendMail = async (options) => {
       pass: process.env.SMPT_PASSWORD,
     },
   });
+
   const mailOptions = {
     from: process.env.SMPT_MAIL,
     to: options.email,
     subject: options.subject,
     text: options.message,
   };
+
   await transporter.sendMail(mailOptions);
 };
 
