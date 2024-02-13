b57de80f69f41ef9b848733bb33427d34c8e4229
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 12:03:14 +0100

    Create and write FAQPage.jsx

---
+++ frontend/src/pages/FAQPage.jsx
@@ -0,0 +1,291 @@
+import React, { useState } from "react";
+import Footer from "../components/Layout/Footer";
+import Header from "../components/Layout/Header";
+import styles from "../styles/styles";
+
+const FAQPage = () => {
+  return (
+    <div>
+      <Header activeHeading={5} />
+      <Faq />
+      <Footer />
+    </div>
+  );
+};
+
+const Faq = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const toggleTab = (tab) => {
+    if (activeTab === tab) {
+      setActiveTab(0);
+    } else {
+      setActiveTab(tab);
+    }
+  };
+
+  return (
+    <div className={`${styles.section} my-8`}>
+      <h2 className="text-3xl font-bold text-gray-900 mb-8">GYIK</h2>
+      <div className="mx-auto space-y-4">
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(3)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Rendelés követése
+            </span>
+            {activeTab === 3 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 3 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                A SocialCommerce oldalán a profilodban a "Rendelés követése"
+                opcióval ellenőrizheted, hogy az eladó át adta e már a csomagot
+                egy futárszolgálatnak.
+                <br></br>A futárszolgálat (mely a szállítást végzi) egy e-mailt
+                küld neked a megrendelt termékkel kapcsolatban.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(4)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Kapcsolatfelvétel
+            </span>
+            {activeTab === 4 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 4 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                Rendeléssel, fiókkal, bejelentkezéssel, regisztrációval és
+                egyéb, a SocialCommerce-el kapcsolatos problémák esetén, kérjük,
+                vedd fel a kapcsolatot az ügyfélszolgálatunkkal a következő
+                e-mail címen: info.socailcommerce@gmail.com
+                <br></br>
+                Termékekkel kapcsolatos kérdések, problémák esetén vedd fel a
+                kapcsolatot az eladóval a termék adatlapján található eladói
+                profil segítségével.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(5)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Rendelés törlése
+            </span>
+            {activeTab === 5 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 5 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                A SocialCommerce-en keresztül leadott rendelések
+                visszamondhatóak a rendelés napján. Ehhez kérjük, vedd fel a
+                kapcsolatot az eladóval.
+                <br></br>A kiszállított, illetve a sikeresen átvett rendelések
+                visszaküldésére nincs lehetőség. Kérjük, még a futár
+                jelenlétében (a sikeres fizetés előtt) vizsgáld meg a csomagot.
+                Ha a csomag sérült, akkor lehetőség van elállni a rendelés
+                átvételétől (ez esetben a csomag visszakerül a feladóhoz).
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(6)}
+          >
+            <span className="text-lg font-medium text-gray-900">Szállítás</span>
+            {activeTab === 6 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 6 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                A házhoz szállítás díját, illetve a külföldre történő szállítást
+                az eladó határozza meg.
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="border-b border-gray-200 pb-4">
+          <button
+            className="flex items-center justify-between w-full"
+            onClick={() => toggleTab(7)}
+          >
+            <span className="text-lg font-medium text-gray-900">
+              Fizetési lehetőségek
+            </span>
+            {activeTab === 7 ? (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          {activeTab === 7 && (
+            <div className="mt-4">
+              <p className="text-base text-gray-500">
+                A SocialCommerce weboldalán keresztül leadott rendelésedet
+                (jelenleg) a futárnál tudod kifizetni készpénzzel (vagy esetleg
+                bankkártyával, ha elérhető ez az opció az adott futárnál).
+                <br></br>A rendelés során csak a "Fizetés a futárnál" opció
+                érhető el.
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQPage;
