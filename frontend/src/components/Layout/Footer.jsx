f09eeb95cce6d23e1e71510262630af9cced4661
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 11:17:31 +0100

    Create and write Footer.jsx

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#000] text-white">
+      <div className="grid grid-cols-1 sm:gird-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 sm:text-center">
+        <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
+          <img src="" alt="" style={{ filter: "brightness(0) invert(1)" }} />
+          <br />
+          <p>Social Commerce</p>
+          <div className="flex items-center mt-[15px]">
+            <AiFillFacebook size={25} className="cursor-pointer" />
+            <AiOutlineTwitter
+              size={25}
+              style={{ marginLeft: "15px", cursor: "pointer" }}
+            />
+            <AiFillInstagram
+              size={25}
+              style={{ marginLeft: "15px", cursor: "pointer" }}
+            />
+            <AiFillYoutube
+              size={25}
+              style={{ marginLeft: "15px", cursor: "pointer" }}
+            />
+          </div>
+        </ul>
+      </div>
+
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+         text-center pt-2 text-gray-400 text-sm pb-8"
+      >
+        <span></span>
+        <span>&copy; 2024 | Social Commerce<br></br>Minden jog fenntartva</span>
+        <div className="sm:block flex items-center justify-center w-full">
+          <img
+            src=""
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
