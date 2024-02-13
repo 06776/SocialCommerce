f96ada717151d5564585825327708a342abbb343
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 12:26:37 +0100

    Create and write ShopRoutes.js

---
+++ frontend/src/routes/ShopRoutes.js
@@ -0,0 +1,19 @@
+import ShopDashboardPage from "../pages/Shop/ShopDashboardPage";
+import ShopCreateProduct from "../pages/Shop/ShopCreateProduct";
+import ShopAllProducts from "../pages/Shop/ShopAllProducts";
+import ShopAllCoupouns from "../pages/Shop/ShopAllCoupouns";
+import ShopPreviewPage from "../pages/Shop/ShopPreviewPage";
+import ShopAllOrders from "../pages/Shop/ShopAllOrders";
+import ShopOrderDetails from "../pages/Shop/ShopOrderDetails";
+import ShopSettingsPage from "../pages/Shop/ShopSettingsPage";
+
+export {
+    ShopDashboardPage,
+    ShopCreateProduct,
+    ShopAllProducts,
+    ShopAllCoupouns,
+    ShopPreviewPage,
+    ShopAllOrders,
+    ShopOrderDetails,
+    ShopSettingsPage,
+}
