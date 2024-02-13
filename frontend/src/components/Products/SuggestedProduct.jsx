fc5a6fadb225327bd120b26a3d73a77f3e00f27a
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 13:35:33 +0100

    Bug fixes in SuggestedProduct.jsx

--- frontend/src/components/Products/SuggestedProduct.jsx
+++ frontend/src/components/Products/SuggestedProduct.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { productData } from "../../static/data";
 import styles from "../../styles/styles";
 import ProductCard from "../Route/ProductCard/ProductCard";
 
 const SuggestedProduct = ({ data }) => {
-  const {allProducts} = useSelector((state) => state.products);
-  const [productData,setProductData] = useState();
+  const { allProducts } = useSelector((state) => state.products);
+  const [productData, setProductData] = useState();
 
   useEffect(() => {
     const d =
-    allProducts && allProducts.filter((i) => i.category === data.category);
+      allProducts && allProducts.filter((i) => i.category === data.category);
     setProductData(d);
-  }, []);
+  }, [allProducts, data.category]);
 
   return (
     <div>
@@ -24,12 +23,11 @@ const SuggestedProduct = ({ data }) => {
             Related Product
           </h2>
           <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-             {
-                productData && productData.map((i,index) => (
-                    <ProductCard data={i} key={index} />
-                ))
-             }
-      </div>
+            {productData &&
+              productData.map((i, index) => (
+                <ProductCard data={i} key={index} />
+              ))}
+          </div>
         </div>
       ) : null}
     </div>
