bb1f32f5a51848700ce0b462bf7f117b0e048574
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 11:47:26 +0100

    Create and write ShopInfo.jsx

---
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -0,0 +1,117 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { server } from "../../server";
+import styles from "../../styles/styles";
+import Loader from "../Layout/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProductsShop } from "../../redux/actions/product";
+
+const ShopInfo = ({ isOwner }) => {
+  const [data, setData] = useState({});
+  const { products } = useSelector((state) => state.products);
+  const [isLoading, setIsLoading] = useState(false);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProductsShop(id));
+    setIsLoading(true);
+    axios
+      .get(`${server}/shop/get-shop-info/${id}`)
+      .then((res) => {
+        setData(res.data.shop);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, [dispatch, id]);
+
+  const logoutHandler = async () => {
+    axios.get(`${server}/shop/logout`, {
+      withCredentials: true,
+    });
+    window.location.reload();
+  };
+
+  const totalReviewsLength =
+    products &&
+    products.reduce((acc, product) => acc + product.reviews.length, 0);
+
+  const totalRatings =
+    products &&
+    products.reduce(
+      (acc, product) =>
+        acc + product.reviews.reduce((sum, review) => sum + review.rating, 0),
+      0
+    );
+
+  const averageRating = totalRatings / totalReviewsLength || 0;
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <div className="w-full py-5">
+            <div className="w-full flex item-center justify-center">
+              <img
+                src={`${data.avatar?.url}`}
+                alt=""
+                className="w-[150px] h-[150px] object-cover rounded-full"
+              />
+            </div>
+            <h3 className="text-center py-2 text-[20px]">{data.name}</h3>
+            <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
+              {data.description}
+            </p>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Cím</h5>
+            <h4 className="text-[#000000a6]">{data.address}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Telefonszám</h5>
+            <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Termékek</h5>
+            <h4 className="text-[#000000a6]">{products && products.length}</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Értékelések</h5>
+            <h4 className="text-[#000000b0]">{averageRating}/5</h4>
+          </div>
+          <div className="p-3">
+            <h5 className="font-[600]">Regisztrálás dátuma</h5>
+            <h4 className="text-[#000000b0]">
+              {data?.createdAt?.slice(0, 10)}
+            </h4>
+          </div>
+          {isOwner && (
+            <div className="py-3 px-4">
+              <Link to="/settings">
+                <div
+                  className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
+                >
+                  <span className="text-white">Profil szerkesztése</span>
+                </div>
+              </Link>
+              <div
+                className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
+                onClick={logoutHandler}
+              >
+                <span className="text-white">Kijelentkezés</span>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ShopInfo;
