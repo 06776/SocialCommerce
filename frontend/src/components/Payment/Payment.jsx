a47383c341b0139ac3a0c223e1ee6d11370a0224
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 11:21:18 +0100

    Create and write Payment.jsx

---
+++ frontend/src/components/Payment/Payment.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../../styles/styles";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+
+const Payment = () => {
+  const [orderData, setOrderData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const orderData = JSON.parse(localStorage.getItem("latestOrder"));
+    setOrderData(orderData);
+  }, []);
+
+  const order = {
+    cart: orderData?.cart,
+    shippingAddress: orderData?.shippingAddress,
+    user: user && user,
+    totalPrice: orderData?.totalPrice,
+  };
+
+  const cashOnDeliveryHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    order.paymentInfo = {
+      type: "Cash On Delivery",
+    };
+
+    await axios
+      .post(`${server}/order/create-order`, order, config)
+      .then((res) => {
+        setOpen(false);
+        navigate("/order/success");
+        toast.success("Order successful!");
+        localStorage.setItem("cartItems", JSON.stringify([]));
+        localStorage.setItem("latestOrder", JSON.stringify([]));
+        window.location.reload();
+      });
+  };
+
+  return (
+    <div className="w-full flex flex-col items-center py-8">
+      <div className="w-[90%] 1000px:w-[70%] block 800px:flex">
+        <div className="w-full 800px:w-[65%]">
+          <PaymentInfo
+            user={user}
+            open={open}
+            setOpen={setOpen}
+            cashOnDeliveryHandler={cashOnDeliveryHandler}
+          />
+        </div>
+        <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
+          <CartData orderData={orderData} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PaymentInfo = ({ user, open, setOpen, cashOnDeliveryHandler }) => {
+  const [select, setSelect] = useState(1);
+
+  return (
+    <div className="w-full 800px:w-[95%] bg-[#fff] rounded-md p-5 pb-8">
+      <br />
+      {/* cash on delivery */}
+      <div>
+        <div className="flex w-full pb-5 border-b mb-2">
+          <div
+            className="w-[25px] h-[25px] rounded-full bg-transparent border-[3px] border-[#1d1a1ab4] relative flex items-center justify-center"
+            onClick={() => setSelect(3)}
+          >
+            {select === 3 ? (
+              <div className="w-[13px] h-[13px] bg-[#1d1a1acb] rounded-full" />
+            ) : null}
+          </div>
+          <h4 className="text-[18px] pl-2 font-[600] text-[#000000b1]">
+            Fizetés a futárnál
+          </h4>
+        </div>
+
+        {/* cash on delivery */}
+        {select === 3 ? (
+          <div className="w-full flex">
+            <form className="w-full" onSubmit={cashOnDeliveryHandler}>
+              <input
+                type="submit"
+                value="Rendelés"
+                className={`${styles.button} !bg-[#f63b60] text-[#fff] h-[45px] rounded-[5px] cursor-pointer text-[18px] font-[600]`}
+              />
+            </form>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+const CartData = ({ orderData }) => {
+  return (
+    <div className="w-full bg-[#fff] rounded-md p-5 pb-8">
+      <div className="flex justify-between">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">Végösszeg:</h3>
+        <h5 className="text-[18px] font-[600]">
+          {orderData?.subTotalPrice} HUF
+        </h5>
+      </div>
+      <br />
+      <div className="flex justify-between border-b pb-3">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">Kedvezmény:</h3>
+        <h5 className="text-[18px] font-[600]">
+          {orderData?.discountPrice ? "HUF" + orderData.discountPrice : "-"}
+        </h5>
+      </div>
+      <h5 className="text-[18px] font-[600] text-end pt-3">
+        {orderData?.totalPrice} HUF
+      </h5>
+      <br />
+    </div>
+  );
+};
+
+export default Payment;
