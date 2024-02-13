41db15bf5d900d0232764f05a5a45fe4c4c34d06
VinLaszlo <vinkovits.laszlo0109@gmail.com>
2024-02-13 11:05:31 +0100

    Create AdminSideBar.jsx

---
+++ frontend/src/components/Admin/Layout/AdminSideBar.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { FiShoppingBag } from "react-icons/fi";
+import { GrWorkshop } from "react-icons/gr";
+import { RxDashboard } from "react-icons/rx";
+import { Link } from "react-router-dom";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { BsHandbag } from "react-icons/bs";
+import { AiOutlineSetting } from "react-icons/ai";
+
+const AdminSideBar = ({ active }) => {
+  return (
+    <div className="w-full h-[90vh] bg-white shadow-sm overflow-y-scroll sticky top-0 left-0 z-10">
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin/dashboard" className="w-full flex items-center">
+          <RxDashboard
+            size={30}
+            color={`${active === 1 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 1 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Főoldal
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-orders" className="w-full flex items-center">
+          <FiShoppingBag
+            size={30}
+            color={`${active === 2 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 2 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Rendelések
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-sellers" className="w-full flex items-center">
+          <GrWorkshop
+            size={30}
+            color={`${active === 3 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 3 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Eladók
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-users" className="w-full flex items-center">
+          <HiOutlineUserGroup
+            size={30}
+            color={`${active === 4 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 4 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Felhasználók
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/admin-products" className="w-full flex items-center">
+          <BsHandbag size={30} color={`${active === 5 ? "crimson" : "#555"}`} />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 5 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Termékek
+          </h5>
+        </Link>
+      </div>
+
+      <div className="w-full flex items-center p-4">
+        <Link to="/profile" className="w-full flex items-center">
+          <AiOutlineSetting
+            size={30}
+            color={`${active === 8 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 8 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Beállítások
+          </h5>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AdminSideBar;
