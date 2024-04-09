7a9920a44d05bfea23a06f01c757fd5e9d657a78
06776 <hajdarapatrik11@outlook.com>
2024-04-09 09:42:47 +0200

    Changes in CORS module

--- backend/app.js
+++ backend/app.js
@@ -3,26 +3,19 @@ const ErrorHandler = require("./middlewares/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    optionsSuccessStatus: 200,
-  })
-);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "config/.env",
-  });
-}
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 const user = require("./controllers/user");
 const shop = require("./controllers/shop");
